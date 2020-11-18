0592fc714e647c55af359143c8663285a5a25436
Jorge Lauro Campos Rodrigues <jorgerodrigues9@outlook.com>
2020-11-18 19:31:13 -0300

    Criado modelos e migrations

---
+++ database/migrations/2020_11_18_221059_create_cats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('age');
+            $table->string('race');
+            $table->string('color');
+            $table->float('price');
+            $table->boolean('sold');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cats');
+    }
+}
